28fbb30cf0795ce39aa06dd3d9e7bf6da0739bce
Tali Rogachev <108060594+Tali74@users.noreply.github.com>
2023-07-12 10:01:56 +0300

    Add files via upload

---
+++ AllPeople/GuestP.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AllPeople
+{
+    public class GuestP : People//נפתח קלאס אורח היורש מאנשים המכיל שם אורח קבוע וכלום כי לאורח אין סיסמה 
+    {
+        public GuestP() : base("Guest", "")
+        {
+
+        }
+    }
+}
